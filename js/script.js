0a0d925008af659c342ced8c6d095a31d6a0d131
mosak <mosak0217@gmail.com>
2022-07-29 17:54:57 +0900

    first commit

--- js/script.js
+++ js/script.js
@@ -1,7 +1,21 @@
-const dt_tags = document.querySelectorAll('.accordion dt');
+var dt_tags = document.querySelectorAll('.accordion dt');
 dt_tags.forEach(dt => {
     dt.addEventListener('click', () => {
         dt.classList.toggle('open');
         dt.nextElementSibling.classList.toggle('open');
     })
 });
+
+
+  var swiper = new Swiper(".swiper-container", {
+    // Optional parameters
+    autoplay: {
+      delay: 3000,
+    },
+    speed: 500,
+    loop: true,
+    setWrapperSize: false,
+    slidesPerView: 3,
+  
+  });
+
