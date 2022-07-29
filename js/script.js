00144a332a6754e34fa2bc17a51e0fa55681c3f5
mosak <mosak0217@gmail.com>
2022-07-30 00:50:47 +0900

    2nd commit

--- js/script.js
+++ js/script.js
@@ -1,10 +1,16 @@
-var dt_tags = document.querySelectorAll('.accordion dt');
+
+'use strict';
+
+
+const dt_tags = document.querySelectorAll('.accordion dt');
 dt_tags.forEach(dt => {
     dt.addEventListener('click', () => {
         dt.classList.toggle('open');
         dt.nextElementSibling.classList.toggle('open');
     })
-});
+
+ 
+
 
 
   var swiper = new Swiper(".swiper-container", {
@@ -16,6 +22,32 @@ dt_tags.forEach(dt => {
     loop: true,
     setWrapperSize: false,
     slidesPerView: 3,
+    loopAdditionalSlides: 1,
+    mousewheel: true,
+      keyboard: true,
+
+      slidesPerView: 1,
+  breakpoints: {
+    // スライドの表示枚数：600px以上の場合
+    600: {
+      slidesPerView: 2,
+    },
+    // スライドの表示枚数：768px以上の場合
+    1025: {
+      slidesPerView: 3,
+    },
+    // スライドの表示枚数：1025px以上の場合
+    1025: {
+      slidesPerView: 4,
+    },
+    // スライドの表示枚数：1025px以上の場合
+    1140: {
+      slidesPerView: 5,
+    },
+  },
   
   });
+});
+
+
 
